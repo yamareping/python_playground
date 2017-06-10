4d19e2fab891d831b1cc789da84be75b7ace66e2
yamareping <yamareping@gmail.com>
2017-06-11 01:10:54 +0800

    First commit of HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+import sys
+
+print("Please enter your height:")
+height = int(sys.stdin.readline())
+
+print("Please enter your weight:")
+weight = int(sys.stdin.readline()) 
+
+msg = "Height: {}, Weight: {}".format(height, weight)
+print(msg)
+
+bmi = weight / ((height / 100) * (height / 100))
+print("BMI: {}".format(bmi))
